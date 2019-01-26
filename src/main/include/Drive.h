22a9a642c42d942ef54722f12a957b58e4842da9
Robin-Parmentier <Robin-Parmentier@users.noreply.github.com>
2019-01-26 10:49:04 -0700

    Added TalonSRX capabilities to muggsy

--- src/main/include/Drive.h
+++ src/main/include/Drive.h
@@ -2,7 +2,7 @@
 #define DRIVE_H
 
 #include <Drive/MecanumDrive.h>
-#include <Talon.h>
+#include <ctre/Phoenix.h>
 
 class Drive{
     public:
@@ -11,15 +11,10 @@ class Drive{
         void mechanum(double x, double y, double z, double throttle);
 
     private:
-        static constexpr int kFrontLeftChannel = 0;
-        static constexpr int kRearLeftChannel = 1;
-        static constexpr int kFrontRightChannel = 2;
-        static constexpr int kRearRightChannel = 3;
-
-        frc::Talon m_frontLeft{kFrontLeftChannel};
-        frc::Talon m_rearLeft{kRearLeftChannel};
-        frc::Talon m_frontRight{kFrontRightChannel};
-        frc::Talon m_rearRight{kRearRightChannel};
+        WPI_TalonSRX m_frontLeft{1};
+        WPI_TalonSRX m_rearLeft{2};
+        WPI_TalonSRX m_frontRight{3};
+        WPI_TalonSRX m_rearRight{4};
         frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
                                  m_rearRight};
 };
