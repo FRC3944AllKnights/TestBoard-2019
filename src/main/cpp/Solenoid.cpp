1b5fe7b1e62e00c4f2732e9300903c26b25fcde3
andre <andrechavez17@gmail.com>
2019-01-26 15:00:42 -0700

    added solenoid functionality

--- src/main/cpp/Solenoid.cpp
+++ src/main/cpp/Solenoid.cpp
@@ -4,7 +4,15 @@ Solenoid::Solenoid(){
 
 }; //consturcter - lets the class be called in this file
     
-void Solenoid::Release(bool on){
+void Solenoid::Release(bool on)  {
+    if(on) {
+        exampleDouble.Set(frc::DoubleSolenoid::Value::kForward);
+    }
+    else {
+        exampleDouble.Set(frc::DoubleSolenoid::Value::kOff);
+    }
+
+
 
 };
 
